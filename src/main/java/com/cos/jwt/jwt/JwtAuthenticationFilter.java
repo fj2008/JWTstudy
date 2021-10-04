d1f4fe54243b349a642f0ebfb4bf57ace4f897b0
unknown <fj2008@naver.com>
2021-10-04 21:29:47 +0900

    JWT토큰 만들기 구현완료

--- src/main/java/com/cos/jwt/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,7 @@
 package com.cos.jwt.jwt;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -14,6 +14,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -63,7 +65,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 			// 임시로 리턴을 해줄authentication 을 만들었다
 			PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-			System.out.println(principalDetails.getUser().getUsername());//여기에 값이 있다는것은 로그인이 정상적으로 됐다는것이다.
+			System.out.println("로그인 완료됨 : " + principalDetails.getUser().getUsername());//여기에 값이 있다는것은 로그인이 정상적으로 됐다는것이다.
 
 			//authentication 객체가 session영역에 저장을 해야하고 그 방법은 return해주면된다.
 			//굳이 리턴을 하는 이유는 권한관리를 security가 대신 해주기때문에 현리하려고 하는것임
@@ -84,7 +86,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
+	
 		System.out.println("successfulAuthentication 실행됨 : 인증이 완료되었다는 뜻임.");
+		PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+		//principalDetails 객체에 들어있는 정보를 사용해서 jwt토큰을 만들것이다.
+		
+		
+		//RSA방식은 아니고 Hash방식암호방식
+		String jwtToken = JWT.create()
+				.withSubject("cos토큰")//토큰이름
+				.withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+				//withExpiresAt은 토큰 만료시간을 설정하는것이다. 
+				//토큰이 탈취가되더라도 일정시간이 되서 만료되면 해당토큰을 사용할 수 없도록한다
+				//currentTimeMillis+만료시간
+				.withClaim("id",principalDetails.getUser().getId())
+				//withClaim는 비공개 클레임을 넣는것인데 여기엔 넣고싶은 (키,벨류)값을 넣어주면 된다.
+				.withClaim("username", principalDetails.getUser().getUsername())
+				.sign(Algorithm.HMAC512("cos"));
+				//sign값은 내 서버만 아는 고유값으로 인증을하는것임
+				//HMAC512의 특징은 서버의 고유한키값을 가지고 있어야하기때문이다.
+		
+		response.addHeader("Authorization","Bearer "+jwtToken);
+		//Bearer를 적어주고 꼭 한칸띄어야한다. 
+		//이렇게 설정하면 Authorization라는 해더에 토큰이 담겨서 응답이 될것이다.
+		
 		super.successfulAuthentication(request, response, chain, authResult);
 	}
 }
